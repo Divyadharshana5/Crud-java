c7568ca9b56c9265701eb05d55302b644a93b1ca
Divyadharshana5 <divyadharshna3@gmail.com>
2026-03-03 16:35:27 +0530

    updated

---
+++ src/main/java/dev/code/Helloworld/HelloWorldController.java
@@ -0,0 +1,12 @@
+package dev.code.Helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping("/hello")
+    String sayHelloWorld(){
+        return "Hello World";
+    }
+}
