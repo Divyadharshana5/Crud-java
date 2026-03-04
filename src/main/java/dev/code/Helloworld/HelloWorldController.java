9a0166191ea67f512de603cddd3d8e438a4b7008
Divyadharshana5 <divyadharshna3@gmail.com>
2026-03-04 10:46:29 +0530

    updated

--- src/main/java/dev/code/Helloworld/HelloWorldController.java
+++ src/main/java/dev/code/Helloworld/HelloWorldController.java
@@ -1,9 +1,11 @@
 package dev.code.Helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1")
 public class HelloWorldController {
     @GetMapping("/h")
     String sayHelloWorld(){
--- src/main/java/dev/code/Helloworld/TodoController.java
+++ src/main/java/dev/code/Helloworld/TodoController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/todo")
+@RequestMapping("/api/v1/todo")
 public class TodoController {
     @GetMapping("/get")
         String getTodo(){
