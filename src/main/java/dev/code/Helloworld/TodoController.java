3920f7abc4034541815c34403d6f9af2a7aea79c
Divyadharshana5 <divyadharshna3@gmail.com>
2026-03-04 10:45:01 +0530

    updated

---
+++ src/main/java/dev/code/Helloworld/TodoController.java
@@ -0,0 +1,18 @@
+package dev.code.Helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/todo")
+public class TodoController {
+    @GetMapping("/get")
+        String getTodo(){
+            return "Todo";
+    }
+    @GetMapping("/id")
+    String getTodoById(){
+        return "Todo with Id";
+    }
+}
