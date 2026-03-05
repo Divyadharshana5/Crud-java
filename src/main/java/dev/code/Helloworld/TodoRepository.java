621dd2f57f5e0173ccfe8d988e392d87ea8cd870
Divyadharshana5 <divyadharshna3@gmail.com>
2026-03-05 10:20:29 +0530

    updated

--- src/main/java/dev/code/Helloworld/TodoRepository.java
+++ src/main/java/dev/code/Helloworld/TodoRepository.java
@@ -1,5 +1,9 @@
 package dev.code.Helloworld;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Bean
 public class TodoRepository {
     String getAllTodos(){
         return "Todo";
