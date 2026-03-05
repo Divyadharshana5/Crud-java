cb5d7e891eeb992cbc977827185e318ffb352961
Divyadharshana5 <divyadharshna3@gmail.com>
2026-03-05 09:58:25 +0530

    updated

--- src/main/java/dev/code/Helloworld/TodoService.java
+++ src/main/java/dev/code/Helloworld/TodoService.java
@@ -1,4 +1,13 @@
 package dev.code.Helloworld;
 
 public class TodoService {
+    private TodoRepository todoRepository;
+
+    public TodoService(){
+        todoRepository=new TodoRepository();
+    }
+
+    public void getTodo(){
+
+    }
 }
