aec09bbab53afbb05905d4e0580206a0e8ce93c7
Divyadharshana5 <divyadharshana3@gmail.com>
2026-03-12 12:55:19 +0530

    updated

--- src/main/java/dev/code/Helloworld/SecurityConfig.java
+++ src/main/java/dev/code/Helloworld/SecurityConfig.java
@@ -1,8 +1,14 @@
 package dev.code.Helloworld;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+    @Bean
+public SecurityFilterChain filterChain(HttpSecurity https) throws Exception{
 
+}
 }
