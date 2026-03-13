2d9c8f9f9f43fe014d036e24a209e18a10203cf3
Divyadharshana5 <divyadharshana3@gmail.com>
2026-03-13 13:15:31 +0530

    updated

--- src/main/java/dev/code/Helloworld/utils/JwtUtil.java
+++ src/main/java/dev/code/Helloworld/utils/JwtUtil.java
@@ -1,10 +1,14 @@
 package dev.code.Helloworld.utils;
 
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
 
 @Component
 public class JwtUtil {
     private final String SECRET = "Dharshana";
     private final long EXPIRATION = 1000 * 60;
-
+    private final Key securityKey= Keys.hmacShaKeyFor(SECRET.getBytes(StandardCharsets.UTF_8));
 }
