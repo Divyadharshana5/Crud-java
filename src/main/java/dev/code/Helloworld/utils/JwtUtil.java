7fe360390d60798afa6aa2f981b429702a3b6fc5
Divyadharshana5 <divyadharshana3@gmail.com>
2026-03-13 13:21:50 +0530

    updated

--- src/main/java/dev/code/Helloworld/utils/JwtUtil.java
+++ src/main/java/dev/code/Helloworld/utils/JwtUtil.java
@@ -1,14 +1,26 @@
 package dev.code.Helloworld.utils;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
 
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
+import java.util.Date;
 
 @Component
 public class JwtUtil {
     private final String SECRET = "Dharshana";
     private final long EXPIRATION = 1000 * 60;
-    private final Key securityKey= Keys.hmacShaKeyFor(SECRET.getBytes(StandardCharsets.UTF_8));
+    private final Key secretKey= Keys.hmacShaKeyFor(SECRET.getBytes(StandardCharsets.UTF_8));
+
+    public String generateToken(String email){
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION))
+                .signWith(secretKey, SignatureAlgorithm.HS256)
+
+    }
 }
