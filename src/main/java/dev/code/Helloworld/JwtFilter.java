c2022de31de4e498b902bd84450361fd5be36897
Divyadharshana5 <divyadharshana3@gmail.com>
2026-03-13 14:18:55 +0530

    updated

--- src/main/java/dev/code/Helloworld/JwtFilter.java
+++ src/main/java/dev/code/Helloworld/JwtFilter.java
@@ -1,16 +1,18 @@
 package dev.code.Helloworld;
 
 import dev.code.Helloworld.utils.JwtUtil;
-import io.jsonwebtoken.Jwt;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.List;
 
 @Component
 public class JwtFilter extends OncePerRequestFilter {
@@ -24,7 +26,9 @@ public class JwtFilter extends OncePerRequestFilter {
         if(authHeader!=null && authHeader.startsWith("Bearer ")){
             String token= authHeader.substring(7);
             if(jwtUtil.validateJwtToken(token)){
-
+               String email= jwtUtil.extractEmail(token);
+               var auth=new UsernamePasswordAuthenticationToken(email,null, List.of());
+                SecurityContextHolder.getContext().setAuthentication(auth);
             }
         }
 
