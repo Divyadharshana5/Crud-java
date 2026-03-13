6c5f04727d37626cc0379d55434b649a2dd63bb8
Divyadharshana5 <divyadharshana3@gmail.com>
2026-03-13 13:08:01 +0530

    updated

--- src/main/java/dev/code/Helloworld/utils/JwtUtil.java
+++ src/main/java/dev/code/Helloworld/utils/JwtUtil.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class JwtUtil {
-    private final String SECRET = "";
-    private final long EXPIRATION = 1000*30;
+    private final String SECRET = "Dharshana";
+    private final long EXPIRATION = 1000 * 60;
+
 }
