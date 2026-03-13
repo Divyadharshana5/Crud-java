22fc0ac0f4073834d9698216721a725de7a0b005
Divyadharshana5 <divyadharshana3@gmail.com>
2026-03-13 14:23:45 +0530

    updated

--- src/main/java/dev/code/Helloworld/SecurityConfig.java
+++ src/main/java/dev/code/Helloworld/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
@@ -15,12 +16,15 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+    public SecurityFilterChain filterChain(HttpSecurity http, JwtFilter jwtFilter) throws Exception{
         http
               .csrf(AbstractHttpConfigurer::disable)
               .authorizeHttpRequests((auth)->auth
+                      .requestMatchers("/auth/**").permitAll()
                       .anyRequest().authenticated()
-              );
+              )
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+        ;
         return http.build();
     }
 }
