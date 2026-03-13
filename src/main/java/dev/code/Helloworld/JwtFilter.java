ac5fc24452bc764e0f2863b024e6188f95921e82
Divyadharshana5 <divyadharshana3@gmail.com>
2026-03-13 14:10:40 +0530

    updated

--- src/main/java/dev/code/Helloworld/JwtFilter.java
+++ src/main/java/dev/code/Helloworld/JwtFilter.java
@@ -12,7 +12,13 @@ import java.io.IOException;
 @Component
 public class JwtFilter extends OncePerRequestFilter {
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        String authHeader= request.getHeader("Authorization");
+        if(authHeader!=null && authHeader.startsWith("Bearer ")){
+            String token= authHeader.substring(7);
+        }
 
     }
 }
