a005e331a9cfee576bc2013c01806fd6c0218967
Divyadharshana5 <divyadharshana3@gmail.com>
2026-03-13 13:40:29 +0530

    updated

--- src/main/java/dev/code/Helloworld/controller/AuthController.java
+++ src/main/java/dev/code/Helloworld/controller/AuthController.java
@@ -3,6 +3,8 @@ package dev.code.Helloworld.controller;
 import dev.code.Helloworld.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,9 +17,13 @@ public class AuthController {
 
     private UserRepository userRepository;
     @PostMapping("/register")
-    public String registerUser(@RequestBody Map<String, String> body){
+    public ResponseEntity<String> registerUser(@RequestBody Map<String, String> body){
         String email = body.get("email");
         String password=body.get("password");
+
+        if(userRepository.findByEmail(email).isPresent()){
+            return new ResponseEntity<>("Email already exists" ,HttpStatus.CONFLICT);
+        }
     }
 
     @PostMapping("/login")
