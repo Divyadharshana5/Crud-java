79225d7372f8ed5a55c6251e1a074300099f4e78
Divyadharshana5 <divyadharshana3@gmail.com>
2026-03-13 13:34:17 +0530

    updated

--- src/main/java/dev/code/Helloworld/controller/AuthController.java
+++ src/main/java/dev/code/Helloworld/controller/AuthController.java
@@ -1,5 +1,8 @@
 package dev.code.Helloworld.controller;
 
+import dev.code.Helloworld.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,10 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
+@RequiredArgsConstructor
 public class AuthController {
+
+    private UserRepository userRepository;
     @PostMapping("/register")
     public String registerUser(@RequestBody Map<String, String> body){
-
+        String email = body.get("email");
+        String password=body.get("password");
     }
 
     @PostMapping("/login")
