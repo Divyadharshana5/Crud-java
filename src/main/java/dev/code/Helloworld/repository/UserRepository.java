ef01c3cfb906a1123f98c35d7cc55e473f473278
Divyadharshana5 <divyadharshana3@gmail.com>
2026-03-14 15:33:23 +0530

    updated

--- src/main/java/dev/code/Helloworld/repository/UserRepository.java
+++ src/main/java/dev/code/Helloworld/repository/UserRepository.java
@@ -1,17 +1,16 @@
 package dev.code.Helloworld.repository;
 
-import dev.code.Helloworld.models.User;  // Your User entity
+import dev.code.Helloworld.models.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
 @Repository
-public interface UserRepository<User> extends MongoRepository<User, String> {
+public interface UserRepository extends MongoRepository<User, String> {
+
     Optional<User> findByUsername(String username);
 
-    Optional<Object> findByEmail(String email);
-    // Add other custom queries if needed
+    Optional<User> findByEmail(String email);
+
 }
-
-
-
